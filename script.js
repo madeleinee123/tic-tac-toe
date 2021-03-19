c9d3964c58d7922aa4e072d27c10314fb6e33493
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-03-19 08:38:26 -0500

    the players are able to select their characters

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+class Player{
+    constructor(name = null, icon = null) {
+        this.icon = icon;
+        this.name = name;
+    }
+    setIcon(fileName){
+        this.icon = fileName;
+    }
+    setName(name){
+        this.name = name;
+    }
+}
+
 /*
 * The class TicTacToeGame allows two users to play tic-tac-toe against each other.  It keeps track of the score over
 * multiple games and interacts with the user.
@@ -18,9 +31,11 @@ class TicTacToeGame{
             bottomCenter: [false, "", this.htmlBoard.querySelector("#bottomCenter")],
             bottomRight: [false, "", this.htmlBoard.querySelector("#bottomRight")]
         }
-        this.first = "x"
-        this.turn = "x";
-        this.openBoxes = 9;
+        this.player1 = null;
+        this.player2 = null;
+        this.first = null;
+        this.turn = null;
+        this.filledBoxes = 0;
         this.gameOver = false;
         this.player1Wins = 0;
         this.player2Wins = 0;
@@ -34,22 +49,27 @@ class TicTacToeGame{
     * Return Value: none
     */
     setSquare(position){
-
         if (!this.board[position][0] && !this.gameOver) {
             this.board[position][0] = true;
-            this.board[position][1] = this.turn;
+            this.board[position][1] = this.turn.icon;
             let image = document.createElement("img");
-            image.setAttribute("src", `images/${this.turn}.png`);
+            image.setAttribute("src", `images/${this.turn.icon}.png`);
             this.board[position][2].appendChild(image);
-            this.openBoxes = this.openBoxes - 1;
+            this.filledBoxes = this.filledBoxes + 1;
+            if (this.filledBoxes === 1){
+                let resetButton = document.querySelector(".reset-button");
+                resetButton.style.display = "block";
+            }
             let message = document.querySelector("#message");
             if (!this.checkForWinner()) {
-                if (this.turn === "x") {
-                    this.turn = "o";
-                    message.textContent = "O's Turn!";
-                } else if (this.turn === "o") {
-                    this.turn = "x";
-                    message.textContent = "X's Turn!"
+                if (this.turn === this.player1) {
+                    this.turn = this.player2;
+                    console.log("here i am!");
+                    message.textContent = "Player 2's Turn!";
+                } else if (this.turn === this.player2) {
+                    console.log("here i am again!");
+                    this.turn = this.player1;
+                    message.textContent = "Player 1's Turn!"
                 }
             }
         }
@@ -61,7 +81,8 @@ class TicTacToeGame{
     * Return Value: none
     */
     addListeners(){
-        let squares = this.htmlBoard.querySelectorAll(".square")
+        let squares = this.htmlBoard.querySelectorAll("[type = tic-tac-toe-square]");
+        console.log(squares);
         squares.forEach(square => square.addEventListener("click", (event) => {
             this.setSquare(square.id);
             event.preventDefault();
@@ -75,13 +96,15 @@ class TicTacToeGame{
 
     /*
     * This method is called whenever the user clicks the reset or play again? button.  It sets all this.board,
-    * this.openBoxes, and this.gameOver back to their original states.  It also switches the player that goes first
+    * this.filledBoxes, and this.gameOver back to their original states.  It also switches the player that goes first
     * to the opposite of the previous game and updates the score boxes to reflect who won in the previous round.
     * Parameters: none
     * Return Value: none
     */
     clearBoard(){
-        this.htmlBoard.querySelector(".reset-button").textContent = "Reset";
+        let resetButton = this.htmlBoard.querySelector(".reset-button");
+        resetButton.textContent = "Reset";
+        resetButton.style.display = "none";
         for (let key in this.board){
             this.board[key][0] = false;
             this.board[key][1] = "";
@@ -92,16 +115,16 @@ class TicTacToeGame{
             this.board[key][2].style.backgroundColor = "#E3C0D3";
         }
         let message = document.querySelector("#message");
-        if(this.first === "x"){
-            this.turn = "o";
-            message.textContent = "O's Go First!!";
-            this.first = "o";
-        }else{
-            this.turn = "x";
-            message.textContent = "X's Go First!!";
-            this.first = "x";
+        if(this.first === this.player1 && this.gameOver){
+            this.turn = this.player2;
+            message.textContent = "Player 2 Goes First!!";
+            this.first = this.player2;
+        }else if (this.first === this.player2 && this.gameOver){
+            this.turn = this.player1;
+            message.textContent = "Player 1 Goes First!!";
+            this.first = this.player1;
         }
-        this.openBoxes = 9;
+        this.filledBoxes = 0;
         this.gameOver = false;
     }
 
@@ -114,7 +137,7 @@ class TicTacToeGame{
     *         returns nothing.
     */
     checkForWinner(){
-        let player = this.turn;
+        let player = this.turn.icon;
         let topLeft = this.board.topLeft[1];
         let topCenter = this.board.topCenter[1];
         let topRight = this.board.topRight[1];
@@ -141,11 +164,12 @@ class TicTacToeGame{
             this.reportWinner(["topLeft", "middleCenter", "bottomRight"]);
         }else if ((bottomLeft === middleCenter) && (middleCenter === topRight) && (bottomLeft === player)){
             this.reportWinner(["bottomLeft", "middleCenter", "topRight"]);
-        }else if (this.openBoxes < 1){
+        }else if (this.filledBoxes === 9){
             this.reportWinner();
         }else{
             return false;
         }
+        return true;
     }
 
     /*
@@ -159,20 +183,22 @@ class TicTacToeGame{
     reportWinner(squares = null){
         this.gameOver = true;
         let message = document.querySelector("#message");
+        console.log(message);
         if (squares === null){
             message.textContent = "It's a tie! Play again??";
+            console.log(message);
         }else{
             let winner;
             for(let i = 0; i < 3; i++){
                 this.board[squares[i]][2].style.backgroundColor = "#FFC759";
             }
-            if (this.turn === "x"){
-                winner = "X's";
+            if (this.turn === this.player1){
+                winner = "Player 1";
                 this.player1Wins = this.player1Wins + 1;
                 let winnerScore = document.querySelector("#x-score > h1");
                 winnerScore.textContent = this.player1Wins;
             }else{
-                winner = "O's";
+                winner = "Player 2";
                 this.player2Wins = this.player2Wins + 1;
                 let winnerScore = document.querySelector("#o-score > h1");
                 winnerScore.textContent = this.player2Wins;
@@ -181,8 +207,6 @@ class TicTacToeGame{
         }
         let resetButton = document.querySelector(".reset-button");
         resetButton.textContent = "Play Again?"
-        let infoBox = document.querySelector("#info");
-        infoBox.prepend(message);
     }
 
     /*
@@ -191,9 +215,67 @@ class TicTacToeGame{
     * Return Value: none
     */
     playGame(){
+        console.log(this.turn);
+        console.log(this.first);
         this.addListeners();
     }
-}
 
+    choosePlayer(){
+        let modal = document.querySelector("#character-modal");
+        let button  = document.querySelector(".character-button");
+        let span = document.querySelector(".close");
+        let choices = document.querySelector(".icon-grid").childNodes;
+        let selectButton = document.querySelector(".confirm-character");
+        let header = document.querySelector("#modal-title");
+        let currentChoice = null;
+        let player1 = new Player("Player 1");
+        let player2 = new Player("Player 2");
+        let player1Choice = true;
+        let player2Choice = false;
+
+        button.addEventListener("click", (event) => {
+            modal.style.display = "block";
+            event.preventDefault();
+        });
+        span.addEventListener("click", (event) => {
+            modal.style.display = "none";
+            event.preventDefault();
+        });
+        choices.forEach((choice) => choice.addEventListener("click", (event) => {
+
+            if (currentChoice !== null){
+                currentChoice.style.background = "none";
+            }
+            currentChoice = choice;
+            choice.style.background = "#E3C0D3";
+            if (player1Choice){
+                player1.icon  = choice.id;
+            }else{
+                player2.icon = choice.id;
+            }
+            event.preventDefault();
+        }))
+        selectButton.addEventListener("click", (event) => {
+            if (player2Choice){
+                this.player2 = player2;
+                modal.style.display = "none";
+                event.preventDefault();
+                button.style.display = "none";
+                this.turn = this.player1;
+                this.first = this.player1;
+                let message = document.querySelector("#message");
+                message.textContent = "Player 1 Goes First!"
+                this.playGame();
+
+            }else{
+                this.player1 = player1;
+                header.textContent = "Player 2: Choose Your Icon..."
+                currentChoice.style.background = "#5a4a53"
+                player1Choice = false;
+                player2Choice = true;
+            }
+        })
+    }
+}
 let game = new TicTacToeGame(); // create an instance of the game
-game.playGame(); // activates the interface
+game.choosePlayer(); // activates the interface
